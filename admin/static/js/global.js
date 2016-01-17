50ebd3a3f0df037a537f8ce818c504509756c843
Andrzej Bazior <andrzej.bazior@eniro.com>
2016-01-17 12:05:05 +0100

    tinymce images base path

--- admin/static/js/global.js
+++ admin/static/js/global.js
@@ -1,6 +1,9 @@
 tinymce.init({
   selector: 'textarea',
   height: 500,
+  relative_urls : false,
+  remove_script_host : true,
+  document_base_url : "http://wsmochota.com.pl/",
   plugins: [
     'advlist autolink lists link image charmap print preview anchor',
     'searchreplace visualblocks code fullscreen',
